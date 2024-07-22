6803555046f08db71758cae22254bde14a80b1dd
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-22 14:48:39 +0200

    auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { log } from "console";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
+async function Images(){
 
-export default async function HomePage() {
   const images = await db.query.images.findMany();
-  console.log(images)
-  return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
+return (
+  <div className="flex flex-wrap gap-4">
         {images.map((image)=>(
           <div key={image.id} className="w-48">
             <img src={image.url} />
@@ -17,6 +16,18 @@ export default async function HomePage() {
         ))
       }
       </div>
+
+)
+}
+export default async function HomePage() {
+  return (
+    <main className="">
+    <SignedOut>
+      Please sign in bitch
+    </SignedOut>
+    <SignedIn>
+      <Images />
+    </SignedIn>
     hello (gallery in progress)
     </main>
   );

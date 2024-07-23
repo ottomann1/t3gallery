3e956e0a438c0474b15f1efa6fecee3f41860ded
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-23 11:35:37 +0200

    aaa

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -3,6 +3,7 @@
 import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs"
 import { UploadButton } from "~/utils/uploadthing"
 import { useRouter } from "next/navigation"
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export function TopNav(){
   const router = useRouter();
@@ -14,10 +15,10 @@ export function TopNav(){
     <SignInButton />
     </SignedOut>
     <SignedIn>
-    <UploadButton endpoint="imageUploader" onClientUploadComplete={()=> {
+    {/* <UploadButton endpoint="imageUploader" onClientUploadComplete={()=> {
       router.refresh();
-    }}/>
-
+    }}/> */}
+    <SimpleUploadButton/>
     <UserButton />
     </SignedIn>
     </div>
